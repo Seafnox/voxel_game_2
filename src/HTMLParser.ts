eb5003c3b42c25ad0e64732facbd297726b8cded
Kirill Warp <kirill9990@gmail.com>
2024-01-12 01:09:14 +0300

    feat: html parser returns html element

--- src/HTMLParser.ts
+++ src/HTMLParser.ts
@@ -1,8 +1,8 @@
 export class HTMLParser {
     private parser: DOMParser = new DOMParser();
 
-    parse(html: string): Element {
+    parse(html: string): HTMLElement {
         let doc = this.parser.parseFromString(html, 'text/html');
-        return doc.body.firstChild as Element;
+        return doc.body.firstChild as HTMLElement;
     }
 }
